fcce02d2d75bdfc8466dc7b9f743f50c71598e35
Alessio Palumbo <apalumbo1501@gmail.com>
2025-09-06 02:05:26 +0200

    Navbar continued

--- AskConnectWebsite/src/app/app.ts
+++ AskConnectWebsite/src/app/app.ts
@@ -9,4 +9,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('AskConnectWebsite');
+
+
+
+
+  hoverIn(el: any) {
+        console.log(el.target);
+      el.target.classList.remove('shrink');
+      el.target.classList.add('expand');
+      document.getElementById('navigationMenu')!.style.display = 'block';
+  }
+  
+  hoverOut(el:any ) {
+    console.log(el);
+        el.target.classList.add('shrink');
+        el.target.classList.remove('expand');
+        document.getElementById('navigationMenu')!.style.display = 'none';
+  }
+
 }
